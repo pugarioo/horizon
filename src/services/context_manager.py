bcccd3228894c1021fc7872a5061f305bc6df082
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2026-02-08 00:50:33 +0800

    intial memory manager and remove llama_cpp in requirement

--- src/services/context_manager.py
+++ src/services/context_manager.py
@@ -8,21 +8,34 @@ class MemoryManager:
     def __init__(self) -> None:
         self.client = chromadb.PersistentClient(path=DB_DIR)
 
-        self.collection = self.client.get_or_create_collection('chat_history')
-
         self.embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
-    def store_memory(self, conversation_id: str, content: str | dict) -> None:
+        self.collection = self.client.get_or_create_collection(
+            name='chat_history', 
+            embedding_function=self.embedde
+        )
+
+    def store_memory(self, conversation_id: str, content: str) -> None:
         if isinstance(content, str):
             content = [content]
 
         self.collection.add(
             documents=content,
-            ids=[self._generate_id(conversation_id=conversation_id) for _ in range(len(content))]
+            ids=[self._generate_id(conversation_id=conversation_id) for _ in range(len(content))],
+            metadatas=[{"conversation_id": conversation_id} for _ in range(len(content))]
         )
     
-    def search_context(self, conversatio_id: str, query: str | dict):
-        pass
+    def search_context(self, conversation_id: str, query: str, top_k: int = 3):
+        if isinstance(query, str):
+            query = [query]
+
+        results = self.collection.query(
+            query_texts=query,
+            n_results=top_k,
+            where={'conversation_id': conversation_id}
+        )
+
+        return results
         
     def _generate_id(self, conversation_id: str) -> str:
         timestamp = int(time.time())
@@ -30,3 +43,6 @@ class MemoryManager:
         suffix = uuid.uuid4().hex[:4]
 
         return f"{conversation_id}-{timestamp}-{suffix}"
+        
+    
+
