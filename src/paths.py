c629d7986692ec76e949f6717c9863b79a471856
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2026-02-15 16:08:40 +0800

    rename src folder to app

--- src/paths.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).parent.parent
-
-MODELS_DIR = ROOT_DIR / "models"
-
-MODELS_CONFIG_PATH = MODELS_DIR / "model_config.yaml"
-
-DB_DIR = ROOT_DIR / "db"
